53824a7983da1e636a71b854e6a2d5418eddb5fb
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-10-28 08:28:25 +0100

    working latest joke functionality

--- CornyClient/MainPage.xaml.cs
+++ CornyClient/MainPage.xaml.cs
@@ -11,11 +11,23 @@ namespace CornyClient
             InitializeComponent();
         }
 
-        private void LoadJoke(object sender, EventArgs e)
+        private void LoadRandomJoke(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost/cornyapi/");
             HttpResponseMessage response = client.GetAsync("random").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = response.Content.ReadAsStringAsync().Result;
+                JokeJson joke = JsonConvert.DeserializeObject<JokeJson>(jsonResponse) ?? new JokeJson();
+                JokeLabel.Text = joke.joke;
+            }
+        }
+        private void LoadLatestJoke(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/cornyapi/");
+            HttpResponseMessage response = client.GetAsync("latest").Result;
             if (response.IsSuccessStatusCode)
             {
                 var jsonResponse = response.Content.ReadAsStringAsync().Result;
